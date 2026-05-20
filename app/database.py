1a2f6b2d00bc97d1ba97bcd0fdd1cbe2f869291b
Gustavo Cesar <gustavo.silva1211@outlook.com>
2026-05-20 16:42:14 -0300

    Conexão da base com a aplicação.

--- app/database.py
+++ app/database.py
@@ -1,3 +1,22 @@
-import os from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sesessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABSE_URL = os.getenv('DATABASE_URL')
+
+if not DATABSE_URL:
+    raise ValueError("DATABASE_URL não encontrada no arquivo .env")
+
+engine = create_engine(DATABSE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
