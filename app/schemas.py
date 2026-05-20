3c7715c0b3546d03d076bbb706affdc10d882032
Gustavo Cesar <gustavo.silva1211@outlook.com>
2026-05-20 17:37:21 -0300

    Configuração da API

---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from datetime import date, datetime
+from typing import Optional
+
+class HabitBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    active: Optional[bool] = True
+
+class HabitResponse(HabitBase):
+    id: int
+    name: str
+    description: Optional[str] = None
+    created_at: datetime
+
+    class Config:
+        model_config = {"from_attributes": True}
+
+class HabitLogCreate(BaseModel):
+    habit_id: int
+    done_date: date
+
+class HabitLogResponse(HabitLogCreate):
+    id: int
+    habit_id: int
+    done_date: date
+    created_at: datetime
+
+    class Config:
+        model_config = {"from_attributes": True}
